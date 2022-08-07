a1df8990238f762e2ab63a186d76d9128a80808d
Lincoln Ferreira de Jesus do Carmo <lincoln.carmo@sptech.school>
2022-08-07 20:32:41 -0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/testef/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.testef
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screen1.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class screen1 extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: AppBar(),);
+  }
+
+}
